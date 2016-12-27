e59cade0990420269a368218338babe2f7762169
Jesús Emilio Fernández de Frutos <jesus.fernandez@vass.es>
2016-12-27 21:12:05 +0100

    Initial Commit

---
+++ CipherApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CipherApp
+//
+//  Created by Jesús Emilio Fernández de Frutos on 27/12/2016.
+//  Copyright © 2016 Jesús Emilio Fernández de Frutos. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ CipherApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CipherApp
+//
+//  Created by Jesús Emilio Fernández de Frutos on 27/12/2016.
+//  Copyright © 2016 Jesús Emilio Fernández de Frutos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
